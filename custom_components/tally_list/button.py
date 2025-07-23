5c45fb1598a851b19bec8f8ad1be5334e9fd97f7
Robin Zimmermann <53911689+Spider19996@users.noreply.github.com>
2025-07-23 21:50:14 +0200

    Rename reset button entity pattern

--- custom_components/tally_list/button.py
+++ custom_components/tally_list/button.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.util import slugify
 
 from .const import (
     DOMAIN,
@@ -25,8 +26,10 @@ class ResetButton(ButtonEntity):
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
         self._hass = hass
         self._entry = entry
-        self._attr_name = f"{entry.data[CONF_USER]} Reset"
-        self._attr_unique_id = f"{entry.entry_id}_reset"
+        user = entry.data[CONF_USER]
+        self._attr_name = f"{user} Reset"
+        self._attr_unique_id = f"{entry.entry_id}_reset_tally"
+        self.entity_id = f"button.{slugify(user)}_reset_tally"
 
     async def async_press(self) -> None:
         await self._hass.services.async_call(
