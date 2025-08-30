46435ddb83fb425e733e1d76f7129c191463b035
Robin Zimmermann <53911689+Spider19996@users.noreply.github.com>
2025-08-30 22:39:49 +0200

    chore: bump version to 30.08.2025-2

--- custom_components/tally_list/websocket.py
+++ custom_components/tally_list/websocket.py
@@ -7,7 +7,12 @@ from homeassistant.components import websocket_api
 from homeassistant.exceptions import Unauthorized
 import voluptuous as vol
 
-from .const import DOMAIN, CONF_OVERRIDE_USERS, CONF_PUBLIC_DEVICES
+from .const import (
+    DOMAIN,
+    CONF_OVERRIDE_USERS,
+    CONF_PUBLIC_DEVICES,
+    CONF_USER_PINS,
+)
 
 
 @websocket_api.websocket_command({vol.Required("type"): f"{DOMAIN}/get_admins"})
@@ -46,7 +51,56 @@ async def websocket_is_public_device(
     connection.send_result(msg["id"], {"is_public": person_name in public_devices})
 
 
+@websocket_api.websocket_command(
+    {
+        vol.Required("type"): f"{DOMAIN}/login",
+        vol.Required("user"): str,
+        vol.Required("pin"): str,
+    }
+)
+@websocket_api.async_response
+async def websocket_login(
+    hass: HomeAssistant,
+    connection: websocket_api.ActiveConnection,
+    msg: dict,
+) -> None:
+    """Validate a user's PIN and create a login session."""
+    if connection.user is None:
+        raise Unauthorized
+
+    user_pins = hass.data.get(DOMAIN, {}).get(CONF_USER_PINS, {})
+    sessions = hass.data.setdefault(DOMAIN, {}).setdefault("sessions", {})
+
+    pin = str(msg["pin"])
+    user = msg["user"]
+    success = user_pins.get(user) == pin
+
+    if success:
+        sessions[connection.user.id] = user
+
+    connection.send_result(msg["id"], {"success": success})
+
+
+@websocket_api.websocket_command({vol.Required("type"): f"{DOMAIN}/logout"})
+@websocket_api.async_response
+async def websocket_logout(
+    hass: HomeAssistant,
+    connection: websocket_api.ActiveConnection,
+    msg: dict,
+) -> None:
+    """Clear a previously created login session."""
+    if connection.user is None:
+        raise Unauthorized
+
+    sessions = hass.data.get(DOMAIN, {}).get("sessions", {})
+    sessions.pop(connection.user.id, None)
+
+    connection.send_result(msg["id"], {"success": True})
+
+
 async def async_register(hass: HomeAssistant) -> None:
     """Register Tally List WebSocket commands."""
     websocket_api.async_register_command(hass, websocket_get_admins)
     websocket_api.async_register_command(hass, websocket_is_public_device)
+    websocket_api.async_register_command(hass, websocket_login)
+    websocket_api.async_register_command(hass, websocket_logout)
